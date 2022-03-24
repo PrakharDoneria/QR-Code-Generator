760228d5c69849693825f734197d28744addc09c
Prakhar Doneria <gamesprotec@gmail.com>
2022-03-24 18:49:17 +0530

    Add files via upload

---
+++ qr maker.py
@@ -0,0 +1,12 @@
+import qrcode
+# Link for website
+input_data = "https://protecgames.godaddysites.com/"
+#Creating an instance of qrcode
+qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5)
+qr.add_data(input_data)
+qr.make(fit=True)
+img = qr.make_image(fill='black', back_color='white')
+img.save('qrcode.png')
